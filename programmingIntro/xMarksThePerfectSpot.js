3b82e847080d4840d487fae85b2866de07c0d508
Shane <navysupra@gmail.com>
2022-06-30 21:58:05 +0000

    Added	x-marks the spot

---
+++ programmingIntro/xMarksThePerfectSpot.js
@@ -0,0 +1,19 @@
+const finalPosition = function (moves) {
+  let x = 0;
+  let y = 0;
+  for (let m = 0; m < moves.length; m++){
+    let move = moves[m];
+    if (move === 'north')
+      y++;
+    else if (move === 'south')
+      y--;
+    else if (move === 'east')
+      x++;
+    else if (move === 'west')
+      x--;
+  }
+  return [x,y];
+}
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+console.log(finalPosition(moves));
