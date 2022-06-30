2c91151421db394f71b31c3d8f0a6c4f28bf201e
Shane <navysupra@gmail.com>
2022-06-30 22:15:00 +0000

    Added	ageCalculator.js

---
+++ programmingIntro/ageCalculator.js
@@ -0,0 +1,7 @@
+const ageCalculator = function(name, yearOfBirth, currentYear){
+  return `${name} is ${currentYear - yearOfBirth} years old.`;
+}
+
+console.log(ageCalculator("Suzie", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 2003, 2015));
+console.log(ageCalculator("Ferdinand", 2015, 2015));
