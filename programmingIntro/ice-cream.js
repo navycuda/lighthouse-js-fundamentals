435a34fe3500a1ebdb0cc91c4047746660125120
Shane <navysupra@gmail.com>
2022-06-30 23:00:47 +0000

    Added	scratchpad for the quiz.

---
+++ programmingIntro/ice-cream.js
@@ -0,0 +1,10 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours.slice(0,1).toString())
+console.log(iceCreamFlavours.slice(-1).toString());
+console.log(iceCreamFlavours.length);
