7b38936ffadc30df04aa2bd59bbbbb6e497bbf2f
NavyCuda <navysupra@gmail.com>
2022-07-03 11:52:05 -0700

    Added 	judgeVegetable.js - struggled with array.reduce()

---
+++ programmingIntro/judgeVegetable.js
@@ -0,0 +1,36 @@
+const judgeVegetable = function (veggies, judgedOn){
+
+  return veggies.reduce((acc, cur, ind, arr) =>{
+    if (judgedOn === 'redness')
+      return acc = acc.redness > cur.redness ? acc : cur;
+    else
+      return acc = acc.plumpness > cur.plumpness ? acc : cur;
+  }, {}).submitter;
+}
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  },
+  {
+    submitter: 'Jeanie Pie',
+    redness: 11,
+    plumpness: 1
+  }
+]
+
+const metric = 'plumpness'
+
+console.log(judgeVegetable(vegetables, metric));
