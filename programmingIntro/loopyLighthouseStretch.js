2e632fa3af517a7171e4a6afc9b3374023a6ce4b
Shane <navysupra@gmail.com>
2022-07-03 03:56:03 +0000

    Added	loopyLighthouseStretch.js

---
+++ programmingIntro/loopyLighthouseStretch.js
@@ -0,0 +1,16 @@
+const loopyLighthouse = function (range, multiples, words) {
+  
+  for (let i = range[0]; i <= range[1]; i++)
+  {
+    let output = "";
+    if (i % multiples[0] === 0)
+      output += words[0];
+    if (i % multiples[1] === 0)
+      output += words[1];
+    if (!output)
+      output = i;
+    console.log(output);
+  }
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
