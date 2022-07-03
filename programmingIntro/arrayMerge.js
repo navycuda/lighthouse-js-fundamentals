945e2058a2e6711600e8a0546efbf17811403cd2
NavyCuda <navysupra@gmail.com>
2022-07-03 03:20:44 -0700

    Added	arrayMerge.js

---
+++ programmingIntro/arrayMerge.js
@@ -0,0 +1,8 @@
+const merge = function (array1, array2) {
+  const output = array1.concat(array2).sort();
+  return output;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
