2404892d30af0b5dd378368749055a04e29bc82d
NavyCuda <navysupra@gmail.com>
2022-07-03 02:49:30 -0700

    Open	Some minor changes to garden block calculator.

--- Calculators/gardenBlocks.js
+++ Calculators/gardenBlocks.js
@@ -1,13 +1,20 @@
 const costPerFoot = function (ft) {
   return (20 / 8) * ft;
 }
+const costPerYard = function (y) {
+  // Round up to the nearest full yard
+  y = Math.ceil(y);
+  return y * 40;
+}
 const gardenSegment = function (corners, wallsX, wallsY, lifts, ftX, ftY) {
   const costCorners = corners * 7.61;
   const costWallsX = costPerFoot(ftX) * wallsX;
   const costWallsY = costPerFoot(ftY) * wallsY;
 
+  const yards = (ftX * ftY * (lifts * 0.5)) / 27;
+
   let cost = (costCorners + costWallsX + costWallsY) * lifts;
-  return cost;
+  return [cost,yards];
 }
 
 let garden = 
@@ -24,6 +31,13 @@ let garden =
 ];
 
 let sum = 0;
-for (let i = 0; i < garden.length; i++)
-  sum += garden[i];
-console.log(`$${sum.toLocaleString("en-us")}`);
+let volume = 0;
+for (let i = 0; i < garden.length; i++) {
+  sum += garden[i][0];
+  volume += garden[i][1];
+}
+
+let totalWithTax = sum + costPerYard(volume);
+console.log(`$${sum.toLocaleString("en-us")} in bricks and boards.`);
+console.log(`${volume.toLocaleString("en-us")}yard^3 is $${costPerYard(volume).toLocaleString("en-us")}`);
+console.log(`Total: $${totalWithTax.toLocaleString("en-us")}`);
