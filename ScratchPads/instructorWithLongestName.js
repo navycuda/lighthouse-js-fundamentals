e53ccdd73c2d33dc9a9ffacde8e6e33b5c40452f
Shane <navysupra@gmail.com>
2022-07-08 15:15:35 +0000

    Added 	instructorWithLongestname.js

---
+++ ScratchPads/instructorWithLongestName.js
@@ -0,0 +1,18 @@
+const instructorWithLongestName = function(instructors) {
+  // Put your solution here
+  return instructors.reduce((a,c) => 
+    c.name.length > a.name.length ? c : a
+  );
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
