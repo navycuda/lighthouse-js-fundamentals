a93d963f5c39ccb4e6e151b7549d346b2eca30a3
Shane <navysupra@gmail.com>
2022-07-08 17:11:08 +0000

    Added	repeatNumbers.js

---
+++ ScratchPads/repeatNumbers.js
@@ -0,0 +1,16 @@
+const repeatNumbers = function(data) {
+  // Put your solution here
+  let result = "";
+  for (let d = 0; d < data.length; d++)
+  {
+    for (let n = 0; n < data[d][1]; n++)
+      result += data[d][0];
+    if (data.length > 0 && d < (data.length - 1))
+      result += ", ";
+  }
+  return result;
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
