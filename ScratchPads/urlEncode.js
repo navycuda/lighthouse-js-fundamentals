3239e24a690cbc7ba453d35ec8f8cfab3af5203a
Shane <navysupra@gmail.com>
2022-07-08 16:15:41 +0000

    Added	urlEncode.js

---
+++ ScratchPads/urlEncode.js
@@ -0,0 +1,11 @@
+const urlEncode = function(text) {
+  // Put your solution here
+  let output = ""
+  for (let t = 0; t < text.length; t++)
+    output += text[t] === ' ' ? '%20' : text[t]
+  return output;
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
