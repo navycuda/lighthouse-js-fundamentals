7db3c483df26f4be90d1994fb9e7a3c57c857bb5
NavyCuda <navysupra@gmail.com>
2022-07-13 01:21:55 -0700

    Added urlDecode.js

---
+++ ScratchPads/changeCalculator.js
@@ -0,0 +1,63 @@
+const calculateChange = function(total, cash) {
+  // Your code here
+  let correctChange = {
+    remainder: cash - total
+  };
+  // Check the denominations
+  getDenominations(correctChange); 
+  // Remove the remainder
+  delete correctChange.remainder;
+  // Return the result
+  return correctChange;
+};
+
+const getDenominations = function(obj){
+  const denominations = [2000,1000,500,200,100,25,10,5,1]
+  for (let d = 0; d < denominations.length; d++)
+  {
+    let divisor = denominations[d];
+    let result = 0;
+    if (obj.remainder > divisor){
+      result = Math.floor(obj.remainder / divisor);
+      obj.remainder -= (divisor * result);
+      // Add correct bill type:
+      if (result > 0){
+        switch (divisor){
+          case 2000:
+            obj.twentyDollar = result;
+            break;
+          case 1000:
+            obj.tenDollar = result;
+            break;
+          case 500:
+            obj.fiveDollar = result;
+            break;
+          case 200:
+            obj.twoDollar = result;
+            break;
+          case 100:
+            obj.oneDollar = result;
+            break;
+          case 25:
+            obj.quarter = result;
+            break;
+          case 10:
+            obj.dime = result;
+            break;
+          case 5:
+            obj.nickle = result;
+            break;
+          case 1:
+            obj.penny = result;
+            break;
+        }
+      }
+    }
+  }
+}
+
+
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
---
+++ ScratchPads/talkingCalender.js
@@ -0,0 +1,57 @@
+const talkingCalendar = function(date) {
+  // Your code here
+  // YYYY/MM/DD
+
+  const year = date.substring(0,4);
+  const month = date.substring(5,7);
+  const day = date.substring(8,10);
+
+  return `${getMonthName(month)} ${getDay(day)}, ${year}`;
+};
+const getMonthName = function(month) {
+  switch (month) {
+    case "01":
+      return 'January';
+    case "02":
+      return 'Feburary';
+    case "03":
+      return 'March';
+    case "04":
+      return 'April';
+    case "05":
+      return 'May';
+    case "06":
+      return 'June';
+    case "07":
+      return 'July';
+    case "08":
+      return 'Augest';
+    case "09":
+      return 'September';
+    case "10":
+      return 'October';
+    case "11":
+      return 'Novemeber';
+    case "12":
+      return 'December';
+  }
+  return "Invalid Month";
+}
+const getDay = function(day) {
+  if (day === "01" || day === "21")
+    day += "st";
+  else if (day === "02" || day === "22")
+    day += "nd";
+  else if (day === "03" || day == "23")
+    day += "rd";
+  else
+    day += "th";
+  
+  if (day[0] === '0')
+    day = day.replace('0', '');
+  return day;
+}
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
