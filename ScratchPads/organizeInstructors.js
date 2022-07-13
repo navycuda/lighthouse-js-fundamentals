84bd283dad953387b33069c2cebd46a2a188eaf4
Shane <navysupra@gmail.com>
2022-07-13 01:55:16 +0000

    Added	organizeInstructors.js

---
+++ ScratchPads/organizeInstructors.js
@@ -0,0 +1,25 @@
+const organizeInstructors = function(instructors) {
+  // Put your solution here
+  courses = {};
+
+  for (let i = 0; i < instructors.length; i++){
+    const bob = instructors[i];
+    if(!courses.hasOwnProperty(bob.course))
+      courses[bob.course] = [];
+    courses[bob.course].push(bob.name);
+  }
+  return courses;
+};
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
